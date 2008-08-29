c41241b498cbde12c067b165ab5f6ae301388ea4
David Baldwin <baldwindavid@gmail.com>
2008-08-29 17:04:14 -0400

    initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+Online Documentation located at [http://www.baldwindev.com/iconoline/documentation.html](http://www.baldwindev.com/iconoline/documentation.html)
+
+**Note:** If you download the repository the documentation is in "documentation.html" at the root of the repo.
---
+++ javascripts/iconoline.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+  // parse hyperlinks for document types to automatically include icon
+  $('a[href^="mailto:"]').addClass('icon-silk-email')
+  $('a[href$=".pdf"]').addClass('icon-silk-page_white_acrobat')
+  $('a[href$=".doc"]').addClass('icon-silk-page_white_word')
+  $('a[href$=".xls"]').addClass('icon-silk-page_white_excel')
+   
+
+  // find all elements with a class matching the "icon-folder_name-image_name" convention
+  $('*').filter (function(){
+    return $(this).attr('className').match(/icon-(\w*)-(\w*)/);
+  }).each( function(){ 
+    var i = 0
+    var classNames = $(this).attr('className').split(' ')
+    var valid_class = ''
+    while(i <= classNames.length - 1){ 
+      if(classNames[i].match(/icon-(\w*)-(\w*)/)){
+        valid_class = classNames[i]
+      }
+      i = i + 1
+    }
+    var icon_path = valid_class.split('-')
+    
+    // add the appropriate background image
+    // Note:  You can choose to point to png or gif (or anything else)
+    $(this).css('background-image','url(images/icons/' + icon_path[1] + '/gif/' + icon_path[2] + '.gif)')
+    
+    // add the "iconic" class which will set up appropriate spacing, padding, margins, etc.
+    $(this).addClass('iconic')
+    
+  })
+
+
+  // disable auto icon behavior for a single element
+  $('.no-icon').css({backgroundImage:"none", margin:0, padding:0});
+
+  // disable the auto icon behavior for an entire table
+  $('table.no-icons a').css({backgroundImage:"none", margin:0, padding:0});
+
+});
